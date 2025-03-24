8ebdf1bf133d3587315d2cbb7f152df62d04c804
nyel-101 <dandevjr@gmail.com>
2025-03-24 17:37:44 +0000

    feat(worker): decouple auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4d83ceb5627466893673028d48432f3 = () => {
-  return '2026-05-06T19:09:56Z';
+const node_14fb5d047b4b46308f14ec6972cfb4ed = () => {
+  return '2025-03-24T17:37:44Z';
 };
