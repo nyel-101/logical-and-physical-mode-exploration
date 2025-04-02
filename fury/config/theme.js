3b4dec99783379e0471db62e5ad77146e6f74c78
nyel-101 <dandevjr@gmail.com>
2025-04-02 01:41:18 +0000

    feat(auth): sanitize payload parser

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b541114f21974abeadd32625ef8c2d55 = () => {
-  return '2026-03-01T20:28:56Z';
+const node_4e470a61c09a4f4f911d84d71457f023 = () => {
+  return '2025-04-02T01:41:18Z';
 };
