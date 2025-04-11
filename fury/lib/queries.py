1a45aa42ace04ef20be2a33d24533a59ec465749
nyel-101 <dandevjr@gmail.com>
2025-04-11 11:04:29 +0000

    test(api): refactor view templates

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7795639f586646a9a3b0c75b3ad2082d = () => {
-  return '2026-06-13T20:25:17Z';
+const node_ead187d878774db0a46e27a3334ca57e = () => {
+  return '2025-04-11T11:04:29Z';
 };
