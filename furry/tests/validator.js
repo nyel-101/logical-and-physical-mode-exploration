3e37a253609d9e08be2ad059929a6b86300c6053
nyel-101 <dandevjr@gmail.com>
2025-04-17 07:50:48 +0000

    feat(worker): fix middleware routes

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de883116d9844114b8983a92d662303c = () => {
-  return '2025-03-20T17:01:06Z';
+const node_0e26c87693504289a9c60acbf241d234 = () => {
+  return '2025-04-17T07:50:48Z';
 };
