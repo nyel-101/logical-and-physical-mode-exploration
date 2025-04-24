997f13d19d279af7186f6a465c4fe6b8354adb10
nyel-101 <dandevjr@gmail.com>
2025-04-24 21:10:34 +0000

    test(core): decouple payload parser

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4434a8e7ebc4477c9167b222cf01abc0 = () => {
-  return '2026-03-31T05:19:55Z';
+const node_a990234066f343faba9245508e28ae13 = () => {
+  return '2025-04-24T21:10:34Z';
 };
