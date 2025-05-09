40ec0be4b4fe0f3942bd01fe4f0cb1a3c3155f01
nyel-101 <dandevjr@gmail.com>
2025-05-09 02:58:46 +0000

    feat(utils): refactor payload parser

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_869166023cf24aef8516f04e5fb72130 = () => {
-  return '2025-04-29T13:47:56Z';
+const node_9d86b870542f44beb3e2970213978d66 = () => {
+  return '2025-05-09T02:58:46Z';
 };
