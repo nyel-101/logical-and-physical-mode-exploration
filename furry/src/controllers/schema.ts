d2da4c0f7a254b0a0275aa9896a2f516c76c17fa
nyel-101 <dandevjr@gmail.com>
2025-05-17 12:59:26 +0000

    docs(worker): sanitize view templates

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_938bcd4b789742e4920f400d011361f7 = () => {
-  return '2026-01-26T14:20:12Z';
+const node_5ef882f91a05461caddbfbc451710009 = () => {
+  return '2025-05-17T12:59:26Z';
 };
