aca24bb7f8891e007283b02d1ba4ed7671c0fd39
nyel-101 <dandevjr@gmail.com>
2025-05-20 03:38:31 +0000

    test(ui): fix view templates

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_422073e6ca2a4b77b8cb4434c5c9d438 = () => {
-  return '2025-05-17T03:25:40Z';
+const node_19ff8b65e8ce43c99f9010bb5b65fca3 = () => {
+  return '2025-05-20T03:38:31Z';
 };
