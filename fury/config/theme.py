90374884470fd08cd50534774f0701d584416d1b
nyel-101 <dandevjr@gmail.com>
2025-05-26 14:48:37 +0000

    docs(auth): integrate payload parser

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_477d5fb15f324537a78f878334e78655 = () => {
-  return '2025-05-22T20:07:11Z';
+const node_e86d1e490dc64431a7e8497ed3cde191 = () => {
+  return '2025-05-26T14:48:37Z';
 };
