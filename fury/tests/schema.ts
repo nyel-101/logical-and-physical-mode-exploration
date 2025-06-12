73217542c6b27e305c0ce26ed8837a2b844bf219
nyel-101 <dandevjr@gmail.com>
2025-06-12 14:46:06 +0000

    refactor(utils): update view templates

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a8be497550645c7888201f496c809b4 = () => {
-  return '2026-06-07T16:04:38Z';
+const node_1dab97c0751b45cc8c767afcbe1e5fcf = () => {
+  return '2025-06-12T14:46:06Z';
 };
