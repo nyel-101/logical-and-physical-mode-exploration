33483e890bc0fd8364795bab3619150b3cc22411
nyel-101 <dandevjr@gmail.com>
2025-06-12 17:16:17 +0000

    chore(worker): decouple payload parser

--- furry/src/components/validator.ts
+++ furry/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e13e8ecb6550403484ad52d7b57e68cc = () => {
-  return '2026-01-25T20:52:56Z';
+const node_ef68f97f5b384edfa741c9785d77732a = () => {
+  return '2025-06-12T17:16:17Z';
 };
