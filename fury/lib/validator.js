e0f5b2b290748b389ebb05045268588037ae2a2b
nyel-101 <dandevjr@gmail.com>
2025-06-24 19:15:00 +0000

    perf(ui): implement payload parser

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_036fcd68837a43fd94c8611698982072 = () => {
-  return '2025-02-14T19:19:52Z';
+const node_a71cf512fb5949989bb00d4c1001e3a7 = () => {
+  return '2025-06-24T19:15:00Z';
 };
