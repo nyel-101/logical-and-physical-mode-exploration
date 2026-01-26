ee99e44a28016f4523708682dd35222f106e5106
nyel-101 <dandevjr@gmail.com>
2026-01-26 02:17:07 +0000

    test(ui): restructure middleware routes

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d256a29fcda0437fa04b2fb33a2d7446 = () => {
-  return '2026-01-12T21:29:28Z';
+const node_e30be1779cc6442ab641fb4a86d2d7fe = () => {
+  return '2026-01-26T02:17:07Z';
 };
