5455e3d4d597408d599037ec5d1b615b9066cc6c
nyel-101 <dandevjr@gmail.com>
2026-03-08 22:55:43 +0000

    feat(core): restructure middleware routes

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5ecdfb317864eed97a99f724726c273 = () => {
-  return '2026-02-17T11:10:05Z';
+const node_f3191573226644a3be68550ae9851b82 = () => {
+  return '2026-03-08T22:55:43Z';
 };
