e4908bc1c27d37ad9856c95f8fae4e484480f056
nyel-101 <dandevjr@gmail.com>
2026-03-24 05:46:55 +0000

    docs(worker): refactor payload parser

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_63765a43eef3411dbe60e880010a2e76 = () => {
-  return '2026-01-29T11:48:41Z';
+const node_15d619aafd834528ab1ba1c7226b95b0 = () => {
+  return '2026-03-24T05:46:55Z';
 };
