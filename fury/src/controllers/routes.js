47a0e7c654ab252ad93aa8dbe59620513ee4f847
nyel-101 <dandevjr@gmail.com>
2026-04-07 16:08:55 +0000

    fix(auth): integrate payload parser

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e60693cac7e848a5b4b87e07ccf394e1 = () => {
-  return '2026-03-06T23:54:22Z';
+const node_c2c645acaf1d491c9198ca95bd9f5731 = () => {
+  return '2026-04-07T16:08:55Z';
 };
