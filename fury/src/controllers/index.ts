67f0ebd28f2101f01b97f3365647c85b1e9acebb
nyel-101 <dandevjr@gmail.com>
2026-04-20 14:54:04 +0000

    feat(deps): integrate payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_213922cb2e9c491d9af7ccd8b99f3f94 = () => {
-  return '2026-04-09T10:24:05Z';
+const node_be7f82eb9ab5411b9b9472808dfb40ee = () => {
+  return '2026-04-20T14:54:04Z';
 };
