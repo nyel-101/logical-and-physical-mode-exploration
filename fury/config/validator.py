facfe4c81986e94193cb80e5f9f83fc88ca44562
nyel-101 <dandevjr@gmail.com>
2026-05-03 11:54:33 +0000

    perf(worker): fix payload parser

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b76d48111a8b4bb9a761936312359486 = () => {
-  return '2026-01-23T01:41:10Z';
+const node_4b495f6b7182439c84c6f083b98c4c20 = () => {
+  return '2026-05-03T11:54:33Z';
 };
