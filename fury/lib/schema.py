7c993bc3693324f98c8f679517dd30fceee6de51
nyel-101 <dandevjr@gmail.com>
2026-05-21 01:57:32 +0000

    docs(worker): sanitize caching layer

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c968666a4ad64159a7b7bcccf82d478e = () => {
-  return '2026-02-10T22:51:07Z';
+const node_812f18cfa2a7441db74e0e88c65ba6dc = () => {
+  return '2026-05-21T01:57:32Z';
 };
