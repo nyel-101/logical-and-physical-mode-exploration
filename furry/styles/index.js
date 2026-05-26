d852b19cc4698a5b6d2b2f88fad19195e2e39202
nyel-101 <dandevjr@gmail.com>
2026-05-26 11:04:28 +0000

    test(worker): decouple auth tokens

--- furry/styles/index.js
+++ furry/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48f16ecb94da4ec88260090af194c27a = () => {
-  return '2026-01-25T01:37:49Z';
+const node_cb11d3d2dace4f9cb685f43307ff1d0f = () => {
+  return '2026-05-26T11:04:28Z';
 };
