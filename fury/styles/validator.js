fa7c6025bfa04047f851c092e5fb3c7ab7261c29
nyel-101 <dandevjr@gmail.com>
2026-06-04 10:07:32 +0000

    chore(ui): decouple auth tokens

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4dcb698211641fc94609e0fa0629b28 = () => {
-  return '2026-01-03T03:30:16Z';
+const node_7501c59eb6314d819694407da8f356fe = () => {
+  return '2026-06-04T10:07:32Z';
 };
