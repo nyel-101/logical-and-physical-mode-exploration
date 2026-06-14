9d92f3365f455d667dc2c56fe249b1ed440fde23
nyel-101 <dandevjr@gmail.com>
2026-06-14 04:08:21 +0000

    fix(api): integrate view templates

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea38540461734c6a85d4b1ff8b121d1f = () => {
-  return '2026-05-05T08:34:08Z';
+const node_442c9566047d4c61b7f9520f4bf098d2 = () => {
+  return '2026-06-14T04:08:21Z';
 };
