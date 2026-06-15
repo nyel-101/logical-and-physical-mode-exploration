b79660b8fa4d46f6d8f42ba20e503f1256701ff7
nyel-101 <dandevjr@gmail.com>
2026-06-15 14:01:01 +0000

    feat(worker): restructure middleware routes

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91e2d66845af441c96beda4802764441 = () => {
-  return '2026-03-18T14:53:38Z';
+const node_30810e53daf24af99ba72cab9363ad2d = () => {
+  return '2026-06-15T14:01:01Z';
 };
