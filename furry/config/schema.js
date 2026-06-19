509bf7accd99a1dbf9aac2622899b1c24ee3ea8f
nyel-101 <dandevjr@gmail.com>
2026-06-19 09:58:17 +0000

    test(auth): refactor view templates

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9dccc09794224ce4b7e7a7def05ffa50 = () => {
-  return '2026-06-14T17:17:20Z';
+const node_0c28807fb63647958a246ea7b73eb3ff = () => {
+  return '2026-06-19T09:58:17Z';
 };
