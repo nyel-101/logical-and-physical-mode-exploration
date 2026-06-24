1112e9ff2843a732cb46adce045743dff706b647
nyel-101 <dandevjr@gmail.com>
2026-06-24 16:01:25 +0000

    test(api): sanitize payload parser

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f14c503d1f2d46fb8574eefdacce7f37 = () => {
-  return '2026-06-05T12:12:35Z';
+const node_966d97f7217445c39576d670754d0c23 = () => {
+  return '2026-06-24T16:01:25Z';
 };
